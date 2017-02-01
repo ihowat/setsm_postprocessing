ec055468577cde19077cb4835c2659384a4b01be
Ian Howat <ihowat@gmail.com>
2017-02-01 11:02:16 -0500

    add max offset condition/break

--- coregisterdems.m
+++ coregisterdems.m
@@ -7,6 +7,9 @@ function [z2out,p,d0] = coregisterdems(x1,y1,z1,x2,y2,z2,varargin)
 % 2010. z2r is the regiestered DEM, p is the z,x,y transformation
 % parameters and rms is the rms of the transformation in the vertical. 
 
+% Maximum offset allowed
+maxp = 15;
+
 x1=x1(:)';
 y1=y1(:);
 
@@ -67,24 +70,33 @@ while it
    
     if exist('m1','var') && exist('m2','var'); dz(~m2n | ~m1) = NaN; end
     
-    if ~any(~isnan(dz(:))); disp('No overlap'); z2out=z2; p=[NaN;NaN;NaN]; d0=[NaN]; return; end
+    if ~any(~isnan(dz(:))); disp('No overlap'); z2out=z2; p=[NaN;NaN;NaN]; d0=NaN; return; end
 	 
     % filter NaNs and outliers
     n = ~isnan(sx) & ~isnan(sy) & ...
         abs(dz - nanmedian(dz(:))) <= nanstd(dz(:));
     
+    if ~any(n(:))
+            fprintf('regression failure, all overlap filtered\n')
+            p = [NaN;NaN;NaN]; % initial trans variable
+            d0 = NaN;
+            z2out=z2;
+        break
+    end
+    
+
     % get RMSE and break if below threshold
     d1 = sqrt(mean(dz(n).^2));
     
     % keep median dz if first iteration
-    if it == 1; 
+    if it == 1
         meddz=median(dz(n)); 
         d00=sqrt(mean((dz(n)-meddz).^2));
     end
     
     fprintf('rmse= %.3f ',d1)
     
-    if d0 - d1 < .001 || isnan(d0) || isinf(d1) || isnan(d0)
+    if d0 - d1 < .001 || isnan(d0)
         
         fprintf('stopping \n')
         % if fails after first registration attempt, set dx and dy to zero
@@ -96,7 +108,7 @@ while it
         end
         break
     end
-    
+        
     %keep this adjustment
     p = pn;
     d0 = d1;
@@ -107,55 +119,19 @@ while it
 
     % solve for new adustment
     pn = p + X\dz(n);
-
+    
     % display offsets
     fprintf('offset(z,x,y): %.3f, %.3f, %.3f\n',pn')
     
+    if any(abs(pn(2:end)) > maxp)
+        fprintf('maximum horizontal offset reached, returning median vertical offset: %.3f\n',meddz)
+        p=[meddz;0;0]; d0=d00;
+        z2out = z2-meddz;
+        break
+    end
+
     % update iteration vars
     it = it+1;
 end
-
-    
-% mask = interp2(x2 - pn(2),y2 - pn(3),double(mask),...
-%         x1(:)',y1(:),'*linear');
-% mask = mask >= 0.1;
-    
-
-%clear sx sy x2 y2  dz n d1 d0 it X p z2n maskn
-% 
-% %% x,y,z - dependent bias
-% disp('Ramp and Elevation Bias Correction')
-% 
-% % grid coordinates for solver
-% [X1,Y1] = meshgrid(x1,y1);
-% 
-% % difference grids
-% dz = z2 - z1;
-% 
-% % filter NaNs for speed and apply std threshold
-%  n = abs(dz - nanmedian(dz(:))) <= nanstd(dz(:)) & mask;
-% 
-% % build design matrix
-% X = [ones(size(dz(n))),z1(n),X1(n),Y1(n)];
-% 
-% % fit
-% p = X\dz(n);
-% 
-% % display offsets
-% disp(['y0,dz(x,y,z):',num2str(p')])
-% 
-% % apply model
-% z2 = z2 - (p(1) + p(2).*z2 + p(3).*X1 + p(4).*Y1);
-% 
-% % recalc differences
-% dz = z2 - z1;
-% 
-% % display final rmse
-% rmsfinal = rms(dz(~isnan(dz)));
-% fprintf('rmse = %.2f\n',rmsfinal)
-% 
-% pelev = p;
-
-
 
 
