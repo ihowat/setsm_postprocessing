90956db0d2c9270644bd3e452a4266f0459c4a8a
Erik Husby <husby036@umn.edu>
2019-12-22 16:19:06 +1300

    Updated strip metadata parsing

--- compileStripMeta.m
+++ compileStripMeta.m
@@ -59,7 +59,7 @@ for i=1:length(f)
     y{i}=str2num(strrep(c{r},'Y:',''));
     
     
-    r=find(~cellfun(@isempty,strfind(c,'Mosaicking Alignment Statistics (meters)')));
+    r=find(~cellfun(@isempty,strfind(c,'Mosaicking Alignment Statistics')));
     r=r+3;
     A=[];
     while r
@@ -73,6 +73,7 @@ for i=1:length(f)
     if isempty(A);
         Nscenes(i)=1;
     else
+        A(:,1) = round(A(:,1), 2);
         
         avg_rmse(i)=mean(A(:,1));
         med_rmse(i)=median(A(:,1));
