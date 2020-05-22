fc31df8b72307d40ec5c83515ab9547440812958
Ian Howat <ihowat@gmail.com>
2020-05-22 14:18:20 -0400

    make filtering/adjustment independent in each direction

--- adjustOffsets.m
+++ adjustOffsets.m
@@ -4,17 +4,17 @@ function  [dZ,dX,dY,global_ids] = adjustOffsets(offsets,varargin)
 %[dZ,dX,dY,dem_index] = adjustOffsets(offsets) returns the optimal shifts in x,y,z 
 % given the offsets. Inarg "offsets" is a structure with fields:
 % 
-%                     i: [n×1 double] dem 1 index
-%                     j: [n×1 double] dem 2 index
-%                    dz: [n×1 double] z offset (dem 1 - dem 2)
-%                    dx: [n×1 double] x offset (dem 1 - dem 2)
-%                    dy: [n×1 double] y offset (dem 1 - dem 2)
-%                   dze: [n×1 double] z offset 1-sigma error
-%                   dxe: [n×1 double] x offset 1-sigma error
-%                   dye: [n×1 double] y offset 1-sigma error
-%         mean_dz_coreg: [n×1 double] mean diff in z after corgestration
-%       median_dz_coreg: [n×1 double] median diff in z after corgestration
-%        sigma_dz_coreg: [n×1 double] std dev of diff in z after corgestration
+%                     i: [nÃ—1 double] dem 1 index
+%                     j: [nÃ—1 double] dem 2 index
+%                    dz: [nÃ—1 double] z offset (dem 1 - dem 2)
+%                    dx: [nÃ—1 double] x offset (dem 1 - dem 2)
+%                    dy: [nÃ—1 double] y offset (dem 1 - dem 2)
+%                   dze: [nÃ—1 double] z offset 1-sigma error
+%                   dxe: [nÃ—1 double] x offset 1-sigma error
+%                   dye: [nÃ—1 double] y offset 1-sigma error
+%         mean_dz_coreg: [nÃ—1 double] mean diff in z after corgestration
+%       median_dz_coreg: [nÃ—1 double] median diff in z after corgestration
+%        sigma_dz_coreg: [nÃ—1 double] std dev of diff in z after corgestration
 %
 % [...] = adjustOffsets(offsets,'parameter',value) specifies filter values
 % for ignoring pairwise offsets. Paremeters and defaults are:
@@ -91,6 +91,12 @@ offsets.j = local_ids(cj);
 % get the number of DEMs in the stack from the max j of i-j pairs
 Ndems=length(global_ids);
 
+% initialize output
+dZ = nan(Ndems,1);
+dX = nan(Ndems,1);
+dY = nan(Ndems,1);
+
+
 %calculate minumum pairwise errors for each dem
 min_sigma_dz_coreg =  accumarray([offsets.i;offsets.j],[offsets.sigma_dz_coreg;offsets.sigma_dz_coreg],[],@min);
 min_abs_mean_dz_coreg =  accumarray([offsets.i;offsets.j],abs([offsets.mean_dz_coreg;offsets.mean_dz_coreg]),[],@min);
@@ -105,59 +111,107 @@ bad_dems = find(min_sigma_dz_coreg > min_sigma_dz_coregMax | ...
 n = ~ismember(offsets.i,bad_dems) & ~ismember(offsets.j,bad_dems);
 
 % remove pairs missing offsets and filter high errors
-n = n & ~isnan(offsets.dz) & ...
-    abs(offsets.dz) < offsetDiffMax & abs(offsets.dze) < offsetErrMax & ...
-    abs(offsets.dx) < offsetDiffMax & abs(offsets.dxe) < offsetErrMax & ...
-    abs(offsets.dy) < offsetDiffMax & abs(offsets.dye) < offsetErrMax;
+n = n & ~isnan(offsets.dz);
 
-offsets=structfun( @(x) x(n,:), offsets,'uniformoutput',0);
+% filter for maximum offsets and offset errors
+nz = n & abs(offsets.dz) < offsetDiffMax & abs(offsets.dze) < offsetErrMax;
+nx = n & abs(offsets.dx) < offsetDiffMax & abs(offsets.dxe) < offsetErrMax;
+ny = n & abs(offsets.dy) < offsetDiffMax & abs(offsets.dye) < offsetErrMax;
 
-Npairs = length(offsets.dx);
+% apply filter to structures
+offsetsz=structfun( @(x) x(nz,:), offsets,'uniformoutput',0);
+offsetsx=structfun( @(x) x(nx,:), offsets,'uniformoutput',0);
+offsetsy=structfun( @(x) x(ny,:), offsets,'uniformoutput',0);
 
-% find DEMs with no acceptable pairs
-i_missing = setdiff(local_ids,unique(offsets.i));
-j_missing = setdiff(local_ids,unique(offsets.j));
-n_missing = intersect(i_missing,j_missing);
+%% Adjust z offsets
+Npairs = length(offsetsz.dz);
 
-% Build design and weight matrices
-A = zeros(Npairs,Ndems); % initialize design matrix
+if Npairs > 0
+    % find DEMs with no acceptable pairs
+    i_missing = setdiff(local_ids,unique(offsetsz.i));
+    j_missing = setdiff(local_ids,unique(offsetsz.j));
+    n_missing = intersect(i_missing,j_missing);
+    
+    % Build design and weight matrices
+    A = zeros(Npairs,Ndems); % initialize design matrix
+    
+    linearInd = sub2ind([Npairs Ndems], (1:Npairs)', offsetsz.i);
+    A(linearInd) = 1;
+    linearInd = sub2ind([Npairs Ndems], (1:Npairs)', offsetsz.j);
+    A(linearInd) = -1;
+    
+    % remove filtered dems
+    A(:,n_missing) = [];
+    
+    % add delta=0 lines
+    A = [A;diag(ones(1,size(A,2)))];
+    
+    dz = [offsetsz.dz;zeros(size(A,2),1)];
+    dze = [offsetsz.dze; ones(size(A,2),1).*4];
+    wz = 1./dze.^2;
+    n = local_ids;
+    n(n_missing) = [];
+    dZ(n) = (wz.*A)\(wz.*dz);
+end
 
-linearInd = sub2ind([Npairs Ndems], (1:Npairs)', offsets.i);
-A(linearInd) = 1;
-linearInd = sub2ind([Npairs Ndems], (1:Npairs)', offsets.j);
-A(linearInd) = -1;
+%% Adjust x offsets
+Npairs = length(offsetsx.dx);
 
-% remove filtered dems
-A(:,n_missing) = [];
+if Npairs > 0
+    % find DEMs with no acceptable pairs
+    i_missing = setdiff(local_ids,unique(offsetsx.i));
+    j_missing = setdiff(local_ids,unique(offsetsx.j));
+    n_missing = intersect(i_missing,j_missing);
+    
+    % Build design and weight matrices
+    A = zeros(Npairs,Ndems); % initialize design matrix
+    
+    linearInd = sub2ind([Npairs Ndems], (1:Npairs)', offsetsx.i);
+    A(linearInd) = 1;
+    linearInd = sub2ind([Npairs Ndems], (1:Npairs)', offsetsx.j);
+    A(linearInd) = -1;
+    
+    % remove filtered dems
+    A(:,n_missing) = [];
+    
+    % add delta=0 lines
+    A = [A;diag(ones(1,size(A,2)))];
+    dx = [offsetsx.dx;zeros(size(A,2),1)];
+    dxe = [offsetsx.dxe; ones(size(A,2),1).*4];
+    wx = 1./dxe.^2;
+    n = local_ids;
+    n(n_missing) = [];
+    dX(n) = (wx.*A)\(wx.*dx);
+end
 
-% add delta=0 lines
-A = [A;diag(ones(1,size(A,2)))];
-dz = [offsets.dz;zeros(size(A,2),1)];
-dx = [offsets.dx;zeros(size(A,2),1)];
-dy = [offsets.dy;zeros(size(A,2),1)];
+%% Adjust y offsets
+Npairs = length(offsetsy.dy);
 
-dze = [offsets.dze; ones(size(A,2),1).*4];
-dxe = [offsets.dxe; ones(size(A,2),1).*4];
-dye = [offsets.dye; ones(size(A,2),1).*4];
-
-wz = 1./dze.^2;
-wx = 1./dxe.^2;
-wy = 1./dye.^2;
-
-n = local_ids;
-n(n_missing) = [];
-
-dZ = nan(Ndems,1);
-dX = nan(Ndems,1);
-dY = nan(Ndems,1);
-
-dZ(n) = (wz.*A)\(wz.*dz);
-dX(n) = (wx.*A)\(wx.*dx);
-dY(n) = (wy.*A)\(wy.*dy);
-
-
-
-
-
-
-
+if Npairs > 0
+    % find DEMs with no acceptable pairs
+    i_missing = setdiff(local_ids,unique(offsetsy.i));
+    j_missing = setdiff(local_ids,unique(offsetsy.j));
+    n_missing = intersect(i_missing,j_missing);
+    
+    % Build design and weight matrices
+    A = zeros(Npairs,Ndems); % initialize design matrix
+    
+    linearInd = sub2ind([Npairs Ndems], (1:Npairs)', offsetsy.i);
+    A(linearInd) = 1;
+    linearInd = sub2ind([Npairs Ndems], (1:Npairs)', offsetsy.j);
+    A(linearInd) = -1;
+    
+    % remove filtered dems
+    A(:,n_missing) = [];
+    
+    % add delta=0 lines
+    A = [A;diag(ones(1,size(A,2)))];
+    
+    dy = [offsetsy.dy;zeros(size(A,2),1)];
+    dye = [offsetsy.dye; ones(size(A,2),1).*4];
+    wy = 1./dye.^2;
+    n = local_ids;
+    n(n_missing) = [];
+    dY(n) = (wy.*A)\(wy.*dy);
+    
+end
