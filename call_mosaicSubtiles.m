9cc04c8cfee63e8ed504dd2341e557a9afb918ef
husby036 <husby036@umn.edu>
2020-09-18 14:42:54 -0500

    Revert changes to call_mosaicSubtiles.m

--- call_mosaicSubtiles.m
+++ call_mosaicSubtiles.m
@@ -6,13 +6,11 @@ if ismac
     subTileDir=['/Users/ihowat/project/howat.4/earthdem/earthdem_mosaic_testing_1km/',tileName];
     addpath('/Users/ihowat/unity-home/demtools')
     tileDefFile = '/Users/ihowat/unity-home/earthdem/PGC_Imagery_Mosaic_Tiles_Arctic.mat';
-    projstr='polar stereo north';
 else
     
     subTileDir=['/fs/project/howat.4/howat.4/earthdem/earthdem_mosaic_testing_1km/',tileName];
     addpath('/home/howat.4/demtools')
     tileDefFile = '~/earthdem/PGC_Imagery_Mosaic_Tiles_Arctic.mat'; %PGC/NGA Tile definition file
-    projstr='polar stereo north';
 end
 
 if dx == 10
@@ -20,22 +18,22 @@ if dx == 10
     
     [x0,x1,y0,y1]=getTileExtents(tileName,tileDefFile);
     
-    mosaicSubTiles(subTileDir,10,outName,projstr,'extent',[x0,x1,y0,y1]);
+    mosaicSubTiles(subTileDir,10,outName,'extent',[x0,x1,y0,y1]);
     
 else
     
     outName=[subTileDir,'_1_1_2m.mat'];
     [x0,x1,y0,y1]=getTileExtents(tileName,tileDefFile,'quadrant','1_1');
-    mosaicSubTiles(subTileDir,2,outName,projstr,'quadrant','1_1','extent',[x0,x1,y0,y1]);
+    mosaicSubTiles(subTileDir,2,outName,'quadrant','1_1','extent',[x0,x1,y0,y1]);
     
     outName=[subTileDir,'_1_2_2m.mat'];
-    mosaicSubTiles(subTileDir,2,outName,projstr,'quadrant','1_2','extent',[x0,x1,y0,y1]);
+    mosaicSubTiles(subTileDir,2,outName,'quadrant','1_2','extent',[x0,x1,y0,y1]);
     
     outName=[subTileDir,'_2_1_2m.mat'];
-    mosaicSubTiles(subTileDir,2,outName,projstr,'quadrant','2_1','extent',[x0,x1,y0,y1]);
+    mosaicSubTiles(subTileDir,2,outName,'quadrant','2_1','extent',[x0,x1,y0,y1]);
     
     outName=[subTileDir,'_2_2_2m.mat'];
-    mosaicSubTiles(subTileDir,2,outName,projstr,'quadrant','2_2','extent',[x0,x1,y0,y1]);
+    mosaicSubTiles(subTileDir,2,outName,'quadrant','2_2','extent',[x0,x1,y0,y1]);
     
 end
 
