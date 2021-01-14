9959420ee8a20e95b69dcb76c1fa545ffa33910d
husby036 <husby036@umn.edu>
2021-01-14 17:12:00 -0600

    Changes to use new REMA pipeline with existing PGC tile submission scripts

--- initializeMosaic.m
+++ initializeMosaic.m
@@ -1,8 +1,20 @@
-function initializeMosaic(project,tileName,outDir)
+function [meta,landTile] = initializeMosaic(project,tileName,outDir,varargin)
 
 % set to true to remove the subdirectory of subtile .mat files after
 % completion:
 removeSubtileFlag=true;
+
+subTileDir='';
+projection='';
+tileDefFile='';
+stripDatabaseFile='';
+stripsDirectory='';
+waterTileDir='';
+refDemFile='';
+tileqcDir='';
+qcFile='';
+tileParamListFile='';
+returnMetaOnly=false;
 
 % Set Paths and filenames
 if strcmpi(project,'rema')
@@ -47,24 +59,126 @@ elseif strcmpi(project,'arcticdem')
     
 end
 
-subTileDir = [outDir,'/',tileName];
 
-outName=[subTileDir,'_10m.mat'];
-if exist(outName,'file')
-    fprintf('%s exists, skipping\n',outName)
-    return
+fprintf('project = %s\n',project)
+fprintf('tileName = %s\n',tileName)
+fprintf('outDir = %s\n',outDir)
+
+if ~isempty(project)
+
+    subTileDir = [outDir,'/',tileName];
+
+    outName=[subTileDir,'_10m.mat'];
+    if exist(outName,'file')
+        fprintf('%s exists, skipping\n',outName)
+        return
+    end
+
+else
+    fprintf('subTileDir = %s\n',subTileDir)
 end
+
+
+% Parse varargins
+n = find(strcmpi(varargin,'projection'));
+if ~isempty(n)
+    projection = varargin{n+1};
+end
+fprintf('projection = %s\n',projection)
+
+n = find(strcmpi(varargin,'subTileDir'));
+if ~isempty(n)
+    subTileDir = varargin{n+1};
+end
+fprintf('subTileDir = %s\n',subTileDir)
+
+n = find(strcmpi(varargin,'tileDefFile'));
+if ~isempty(n)
+    tileDefFile = varargin{n+1};
+end
+fprintf('tileDefFile = %s\n',tileDefFile)
+
+n = find(strcmpi(varargin,'stripDatabaseFile'));
+if ~isempty(n)
+    stripDatabaseFile = varargin{n+1};
+end
+fprintf('stripDatabaseFile = %s\n',stripDatabaseFile)
+
+n = find(strcmpi(varargin,'stripsDirectory'));
+if ~isempty(n)
+    stripsDirectory = varargin{n+1};
+end
+fprintf('stripsDirectory = %s\n',stripsDirectory)
+
+n = find(strcmpi(varargin,'waterTileDir'));
+if ~isempty(n)
+    waterTileDir = varargin{n+1};
+end
+fprintf('waterTileDir = %s\n',waterTileDir)
+
+n = find(strcmpi(varargin,'refDemFile'));
+if ~isempty(n)
+    refDemFile = varargin{n+1};
+end
+fprintf('refDemFile = %s\n',refDemFile)
+
+n = find(strcmpi(varargin,'tileqcDir'));
+if ~isempty(n)
+    tileqcDir = varargin{n+1};
+end
+fprintf('tileqcDir = %s\n',tileqcDir)
+
+n = find(strcmpi(varargin,'tileParamListFile'));
+if ~isempty(n)
+    tileParamListFile = varargin{n+1};
+end
+fprintf('tileParamListFile = %s\n',tileParamListFile)
+
+n = find(strcmpi(varargin,'returnMetaOnly'));
+if ~isempty(n)
+    returnMetaOnly = varargin{n+1};
+end
+fprintf('returnMetaOnly = %d\n',returnMetaOnly)
+
+
+% attempt to set uninitialized qc file location
+if isempty(qcFile) && ~isempty(tileqcDir)
+    qcFile = [tileqcDir,'/',tileName,'.mat'];
+    fprintf('qcFile = %s\n',qcFile)
+end
+
 
 % load tile definition file into structure
 tileDefs=load(tileDefFile);
 
+if startsWith(tileName,'utm')
+    sl = split(tileName,'_');
+    tilePrefix = [sl{1},'_'];
+    tileName_in_tileDef = strjoin(sl(2:3),'_');
+else
+    tilePrefix = '';
+    tileName_in_tileDef = tileName;
+end
+
+% if projection argument not given, check tile definition
+tileDefs_projection = '';
+if isfield(tileDefs, 'projection')
+    tileDefs_projection = tileDefs.projection;
+    tileDefs = rmfield(tileDefs, 'projection');
+elseif isfield(tileDefs, 'projstr')
+    tileDefs_projection = tileDefs.projstr;
+    tileDefs = rmfield(tileDefs, 'projstr');
+end
+if isempty(projection) && ~isempty(tileDefs_projection)
+    projection = tileDefs_projection;
+end
+
 % find index of tile in tile def database
-tileInd = strcmp(tileDefs.I,tileName);
+tileInd = strcmp(tileDefs.I,tileName_in_tileDef);
 
 % crop tileDefs structure to just this tile
 tileDefs = structfun( @(x) x(tileInd), tileDefs,'uniformoutput',0);
 
-% get tile boundaries with buffer for making land mask and strip search
 % get tile boundaries with buffer for making land mask and strip search
 res=10; % ouput mosaic resolution in meters
 buffer=100; % size of tile/subtile boundary buffer in meters
@@ -106,31 +220,113 @@ else
     landTile = waterTileDir;
 end
 
+% convert argument/tileDefs projection to abbreviated tile projection "tileProjName"
+tileProjName = '';
+if startsWith(tileName,'utm')
+    tileName_parts = strsplit(tileName,'_');
+    tileProjName = tileName_parts{1};
+end
+if ~isempty(projection)
+
+    if strcmpi(projection, 'polar stereo north')
+        tileProjName = 'psn';
+    elseif strcmpi(projection, 'polar stereo south')
+        tileProjName = 'pss';
+    elseif startsWith(projection,'utm', 'IgnoreCase',true)
+        if ~startsWith(tileProjName, projection, 'IgnoreCase',true)
+            error("'projection' argument (%s) and tileName (%s) prefix (%s) do not match", ...
+                projection, tileName, tileProjName);
+        end
+    else
+        error("No matching tileProjName for 'projection' argument: '%s'", tileProjName);
+    end
+end
+
 %%  load the strip database file and search for overlapping strips
+if endsWith(stripDatabaseFile,'.shp', 'IgnoreCase',true)
 
-fprintf('reading database file: %s\n',stripDatabaseFile)
-S=shaperead(stripDatabaseFile);
+    fprintf('reading strip database shapefile: %s\n',stripDatabaseFile)
+    S=shaperead(stripDatabaseFile);
 
-% search for overlapping strips
-n = stripSearch({S.X},{S.Y},x0,x1,y0,y1);
+    % (the following isn't necessary because a strip database in shapefile format
+    %  will only contain strip records that are in the same projection)
+%    if isfield(S,'projection')
+%        % trim strip database to only strips with a projection matching the tile
+%        if isempty(projection)
+%            error("'projection' argument must be provided when strip dbase has 'projection' field");
+%        end
+%        n = strcmpi({S.projection},tileProjName);
+%        if ~any(n)
+%            error("no matches between tileProjName='%s' and strip dbase 'projection' field", tileProjName);
+%        end
+%        S = S(n);
+%    end
 
-% crop structure to this tile
-S = S(n);
+    % search for overlapping strips
+    n = stripSearch({S.X},{S.Y},x0,x1,y0,y1);
 
-%% Create meta input file from shp structure
+    % crop structure to this tile
+    S = S(n);
 
-% make full path filanames for each segment without extension
-meta.fileName = cellfun(@(w,x,y,z) [stripsDirectory,'/',w,'/strips_v4/2m/',x,'_',y,'/',x,'_seg',num2str(z),'_dem_10m.tif'],{S.region},{S.strip},{S.version},...
-    {S.seg_id},'uniformoutput',0);
+    %% Create meta input file from shp structure
 
-% convert shapefile vertices to cells in meta struct, removing nans
-meta.x = cellfun( @(x) x(~isnan(x)), {S.X}, 'uniformoutput', 0);
-meta.y = cellfun( @(x) x(~isnan(x)), {S.Y}, 'uniformoutput', 0);
+    % make full path filanames for each segment without extension
+    meta.fileName = cellfun(@(w,x,y,z) [stripsDirectory,'/',w,'/strips_v4/2m/',x,'_',y,'/',x,'_seg',num2str(z),'_dem_10m.tif'],{S.region},{S.strip},{S.version},...
+        {S.seg_id},'uniformoutput',0);
 
-% make mean scene2strip aligment rmse field
-meta.scene_alignment_meanrmse = [S.rmse]';
+    % convert shapefile vertices to cells in meta struct, removing nans
+    meta.x = cellfun( @(x) x(~isnan(x)), {S.X}, 'uniformoutput', 0);
+    meta.y = cellfun( @(x) x(~isnan(x)), {S.Y}, 'uniformoutput', 0);
 
-meta.scene_alignment_meanrmse(meta.scene_alignment_meanrmse <= 0) = NaN;
+    % make mean scene2strip aligment rmse field
+    meta.scene_alignment_meanrmse = [S.rmse]';
+
+    meta.scene_alignment_meanrmse(meta.scene_alignment_meanrmse <= 0) = NaN;
+
+
+elseif endsWith(stripDatabaseFile,'.mat', 'IgnoreCase',true)
+
+    fprintf('reading strip database .mat file: %s\n',stripDatabaseFile)
+    meta=load(stripDatabaseFile);
+
+    % trim strip database to only strips with a projection matching the tile
+    if isfield(meta,'strip_projection_name')
+        if isempty(projection)
+            error("'projection' argument must be provided when strip dbase has 'strip_projection_name' field");
+        end
+        n = strcmpi(meta.strip_projection_name,tileProjName);
+        if ~any(n)
+            error("no matches between tileProjName='%s' and strip dbase 'strip_projection_name' field", tileProjName);
+        end
+        meta = structfun(@(x) x(n), meta, 'uniformoutput',0);
+    end
+
+    % search for overlapping strips
+    n = stripSearch(meta.x,meta.y,x0,x1,y0,y1);
+    meta = structfun(@(x) x(n), meta, 'uniformoutput',0);
+
+    % make mean scene2strip aligment rmse field
+    if ~isfield(meta,'scene_alignment_meanrmse')
+        if isfield(meta,'avg_rmse')
+            % this is from an old version of the meta files that used 0 in mean
+            error('avg_rmse field in meta structure, needs to be updated')
+        elseif isfield(meta,'scene_alignment')
+            % need to rm zeros (first scene) and nans (unused redundant scenes),
+            % strips w/ 1 scene will be NaN
+            meta.scene_alignment_meanrmse = cellfun(@(x)...
+                mean(x.rmse(x.rmse~=0 & ~isnan(x.rmse))), meta.scene_alignment);
+        else
+            error('missing scene alignment field in meta structure')
+        end
+    end
+
+end
+
+if ~isfield(meta,'A')
+    % get strip areas and alignment stats for quality selection
+    meta.A = cellfun(@(x,y) polyarea(x,y), meta.x,meta.y);
+end
+
 
 %% load qc file if it exists and add to meta struct
 if exist(qcFile,'file')
@@ -155,12 +351,16 @@ else
     fprintf('no qc file found\n')
 end
 
+
+if returnMetaOnly; return; end
+
+
 %% Build and mosaic subtiles
 buildSubTiles(tileName,subTileDir,tileDefs,meta,'landTile',landTile,...
     'refDemFile',refDemFile,'buffer',buffer,'minStripOverlap',...
-    minStripOverlap,'filter',filterFlag)
+    minStripOverlap,'filter',filterFlag,'projection',projection)
 
-call_mosaicSubTiles(subTileDir,10,projection);
+call_mosaicSubTiles(subTileDir,10,projection,tileDefFile);
 
 if removeSubtileFlag
     rmdir(subTileDir, 's')
