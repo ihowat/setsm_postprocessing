705595ed6f706c4c0448c11c709e09d9cf74cf45
husby036 <husby036@umn.edu>
2021-01-14 16:59:03 -0600

    Add detailed PBS job prologue

--- qsub_buildSubTiles.sh
+++ qsub_buildSubTiles.sh
@@ -4,11 +4,35 @@
 #PBS -m n
 #PBS -k oe
 #PBS -j oe
+#PBS -q batch
 
-echo "PBS SUBMISSION DIR: ${PBS_O_WORKDIR}"
-echo "JOB ID: ${PBS_JOBID}"
-echo "NODE: ${PBS_O_HOST}"
-echo "NODE FILE: ${PBS_NODEFILE}"
+echo ________________________________________________________
+echo
+echo PBS Job Log
+echo Start time: $(date)
+echo
+echo Job name: $PBS_JOBNAME
+echo Job ID: $PBS_JOBID
+echo Submitted by user: $USER
+echo User effective group ID: $(id -ng)
+echo
+echo Hostname of submission: $PBS_O_HOST
+echo Submitted to cluster: $PBS_SERVER
+echo Submitted to queue: $PBS_QUEUE
+echo Requested nodes per job: $PBS_NUM_NODES
+echo Requested cores per node: $PBS_NUM_PPN
+echo Requested cores per job: $PBS_NP
+echo Node list file: $PBS_NODEFILE
+echo Nodes assigned to job: $(cat $PBS_NODEFILE)
+echo Running node index: $PBS_O_NODENUM
+echo
+echo Running on hostname: $HOSTNAME
+echo Parent PID: $PPID
+echo Process PID: $$
+echo
+echo Working directory: $PBS_O_WORKDIR
+echo ________________________________________________________
+echo
 
 # move to temp folder to reduce startup time
 mkdir -p ~/matlab_temp
