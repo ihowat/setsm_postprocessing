a56bb3cd496ce1dcf3b650996d0b9c257383b5f4
husby036 <husby036@umn.edu>
2021-05-05 15:01:50 -0500

    Add job preamble for SLURM

--- qsub_buildSubTiles.sh
+++ qsub_buildSubTiles.sh
@@ -47,6 +47,35 @@ echo
 echo Working directory: $PBS_O_WORKDIR
 echo ________________________________________________________
 echo
+
+#echo ________________________________________
+#echo
+#echo SLURM Job Log
+#echo Start time: $(date)
+#echo
+#echo Job name: $SLURM_JOB_NAME
+#echo Job ID: $SLURM_JOBID
+#echo Submitted by user: $USER
+#echo User effective group ID: $(id -ng)
+#echo
+#echo SLURM account used: $SLURM_ACCOUNT
+#echo Hostname of submission: $SLURM_SUBMIT_HOST
+#echo Submitted to cluster: $SLURM_CLUSTER_NAME
+#echo Submitted to node: $SLURMD_NODENAME
+#echo Cores on node: $SLURM_CPUS_ON_NODE
+#echo Requested cores per task: $SLURM_CPUS_PER_TASK
+#echo Requested cores per job: $SLURM_NTASKS
+#echo Requested walltime: $SBATCH_TIMELIMIT
+##echo Nodes assigned to job: $SLURM_JOB_NODELIST
+##echo Running node index: $SLURM_NODEID
+#echo
+#echo Running on hostname: $HOSTNAME
+#echo Parent PID: $PPID
+#echo Process PID: $$
+#echo
+#echo Working directory: $SLURM_SUBMIT_DIR
+#echo ________________________________________________________
+#echo
 set -u
 
 set +u
