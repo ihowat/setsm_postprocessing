62351c830968d5676cd96326e0ebdb74edc9aa95
husby036 <husby036@umn.edu>
2021-07-08 21:47:56 +0800

    Re-enable strip database combiners

--- combineDatabase4_triple.m
+++ combineDatabase4_triple.m
@@ -1,28 +1,28 @@
 function combineDatabase4(dbase1,dbase2,dbase3,dbase_out)
 
-%fprintf("Loading dbase1 as meta1: %s\n", dbase1);
-%meta1 = load(dbase1);
-%fprintf("Loading dbase2 as meta2: %s\n", dbase2);
-%meta2 = load(dbase2);
-%fprintf("Loading dbase3 as meta3: %s\n", dbase3);
-%meta3 = load(dbase3);
-%
-%fprintf("Appending meta2 and meta3 to meta1\n");
-%flds = fields(meta1);
-%i=1;
-%for i = 1:length(flds)
-%    fld = flds{i};
-%    eval(['meta1.',fld,'= [meta1.',fld,',meta2.',fld,',meta3.',fld,'];']);
-%end
-%
-%fprintf("Clearing meta2\n");
-%clear meta2;
-%fprintf("Clearing meta3\n");
-%clear meta3;
-%fprintf("Saving combined meta1 database\n");
-%save(dbase_out,'-struct','meta1','-v7.3');
-%fprintf("Clearing meta1\n");
-%clear meta1;
+fprintf("Loading dbase1 as meta1: %s\n", dbase1);
+meta1 = load(dbase1);
+fprintf("Loading dbase2 as meta2: %s\n", dbase2);
+meta2 = load(dbase2);
+fprintf("Loading dbase3 as meta3: %s\n", dbase3);
+meta3 = load(dbase3);
+
+fprintf("Appending meta2 and meta3 to meta1\n");
+flds = fields(meta1);
+i=1;
+for i = 1:length(flds)
+    fld = flds{i};
+    eval(['meta1.',fld,'= [meta1.',fld,',meta2.',fld,',meta3.',fld,'];']);
+end
+
+fprintf("Clearing meta2\n");
+clear meta2;
+fprintf("Clearing meta3\n");
+clear meta3;
+fprintf("Saving combined meta1 database\n");
+save(dbase_out,'-struct','meta1','-v7.3');
+fprintf("Clearing meta1\n");
+clear meta1;
 
 fprintf("Combining reproject lists\n");
 rlist1 = strrep(dbase1, '.mat', '_reproject_list.txt');
