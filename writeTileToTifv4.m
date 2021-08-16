f0e62e918396eb4c605dcb7186beb3a6267a74da
Erik Husby <husby036@umn.edu>
2021-08-16 17:49:19 +0800

    Use 10 pixel buffer in writing out tiles from both mosaicSubTiles.m and writeTileToTifv4.m

--- writeTileToTifv4.m
+++ writeTileToTifv4.m
@@ -4,6 +4,9 @@ function writeTileToTifv4(tilef,projstr)
 
 fprintf('Source: %s\n',tilef);
 
+% output tile buffer in pixels
+buffer = 10;
+
 % load m file and get coordinate vectors
 m=matfile(tilef);
 x=m.x;
@@ -73,6 +76,14 @@ end
 nx(end) = nx(end)-1;
 ny(end) = ny(end)-1;
 
+% add standard tile buffer
+if buffer > 0
+    nx(1) = max(nx(1)-buffer, 1);
+    nx(2) = min(nx(2)+buffer, length(x));
+    ny(1) = max(ny(1)-buffer, 1);
+    ny(2) = min(ny(2)+buffer, length(y));
+end
+
 %crop coordinate vectors
 x=x(nx(1):nx(2));
 y=y(ny(1):ny(2));
@@ -82,7 +93,7 @@ outNameDem = strrep(tilef,'.mat','_dem.tif');
 if exist(outNameDem,'file')
     fprintf('%s exists, skipping\n',outNameDem);
 else
-    z=m.z(ny(1):ny(2),nx(1):nx(2));
+    z=m.z(ny(1):ny(end),nx(1):nx(end));
     z(isnan(z)) = -9999;
     writeGeotiff(outNameDem,x,y,z,4,-9999,projstr)
     clear z
