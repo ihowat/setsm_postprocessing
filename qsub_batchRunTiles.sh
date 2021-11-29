0f7f5daa9d3870ffe0dd261cc9e50ddebb62f57c
Erik Husby <husby036@umn.edu>
2021-11-29 21:07:51 +0300

    Alter BST+MST workflow to reduce strain on BW scheduler and filesystem - New `--tasks-per-job` option for running multi-node parallel BST(+MST) tile jobs - Use compute node local space for BST+MST subtiles on BW by default

--- qsub_batchRunTiles.sh
+++ qsub_batchRunTiles.sh
@@ -2,12 +2,28 @@
 
 tilelist_string="$ARG_TILENAME"
 tilerun_jobscript="$TILERUN_JOBSCRIPT"
+run_tiles_in_parallel="$IN_PARALLEL"
 
 IFS='@' read -r -a tilelist_arr <<< "$tilelist_string"
 total_ntiles=${#tilelist_arr[@]}
 
+if [ "$run_tiles_in_parallel" = true ]; then
+    echo "Will run ${total_ntiles} tiles in parallel"
+else
+    echo "Will run ${total_ntiles} tiles in serial"
+fi
+
 for tile_idx in "${!tilelist_arr[@]}"; do
     tile="${tilelist_arr[$tile_idx]}"
     echo -e "\n\nRunning tile $((tile_idx+1)) of ${total_ntiles}: ${tile}"
-    bash "$tilerun_jobscript" "$tile"
+    export ARG_TILENAME="$tile"
+    if [ "$run_tiles_in_parallel" = true ]; then
+        bash "$tilerun_jobscript" &
+    else
+        bash "$tilerun_jobscript"
+    fi
 done
+
+if [ "$run_tiles_in_parallel" = true ]; then
+    wait
+fi
