ee4cbee4a10941fa8e46b20cfe20e1d413237a84
Ian Howat <ihowat@gmail.com>
2021-12-06 16:47:37 -0500

    Add files via upload

--- boundaryAdjust.m
+++ boundaryAdjust.m
@@ -22,6 +22,19 @@ if ~iscell(fileNames)
     fileNames = {fileNames};
 end
 
+% find neighbors
+[ntop,nright,ntopRight,nbottomRight] = findNeighborTiles(fileNames);
+
+nN = nan(length(fileNames),8);
+nN(ntop(:,1),1) = ntop(:,2); %top
+nN(ntop(:,2),2) = ntop(:,1); % bottom
+nN(nright(:,2),3) = nright(:,1); %left
+nN(nright(:,1),4) = nright(:,2); %right
+nN(nbottomRight(:,2),5) = nbottomRight(:,1); %top-left
+nN(ntopRight(:,1),6) = ntopRight(:,2); %top-right
+nN(ntopRight(:,2),7) = ntopRight(:,1); %bottom-left
+nN(nbottomRight(:,1),8) = nbottomRight(:,2); %bottom-right
+
 %% dz0 calc and write loop - this could be run simultaneously/parallel
 i=1;
 for i =1:length(fileNames)
@@ -33,8 +46,12 @@ for i =1:length(fileNames)
             continue
     end
     
-    calcdz0(fileNames{i},resizeFraction)
+    neighborFiles = cell(8,1);
+    neighborFiles(~isnan(nN(i,:))) = fileNames(nN(i,~isnan(nN(i,:))));
+    
+    calcdz0(fileNames{i},neighborFiles,resizeFraction)
     fprintf('done\n')
+    clear  neighborFiles
 end
 
 if ~isempty(varargin)
@@ -63,79 +80,190 @@ for i =1:length(fileNames)
             continue
     end
     
-   m0.Properties.Writable = true;
-   m0.z = m0.z - m0.dz0;
-   m0.adjusted = true;
-   fprintf('done\n')
+    
+    s=whos(m0);
+    sz=  s(strcmp({s.name},'z')).size;
+    
+    m0.Properties.Writable = true;
+    dz0 = imresize(m0.dz0,sz);
+    m0.z = m0.z - dz0;
+    m0.adjusted = true;
+    clear dz0
+    fprintf('done\n')
 end
 
-function calcdz0(fileName,resizeFraction)
+function calcdz0(fileName,neighborFiles,resizeFraction)
+% calcdz0 claculate neighbor offset field
+%
+% calcdz0(fileName,neighborFiles,resizeFraction) calculates the z offset
+% over the neighboring files, listed in neighborFiles, and interpolates an
+% offset field over z. The neighborFiles list is in order: top, bottom,
+% left, right, top-left, top-right, bottom-left, bottom-right, with empty
+% cells indicating no file.
 
 m0=matfile(fileName);
 s=whos(m0);
 sz=  s(strcmp({s.name},'z')).size;
 dz0 = nan(ceil(sz.*resizeFraction));
 
-[path,name,ext]=fileparts(fileName);
-
-if isempty(path)
-    path='.';
-end
-
-a = strsplit(name,'_');
-stringLength=num2str(length(a{1}));
-tileRow=str2num(a{1});
-tileCol=str2num(a{2});
-
-formatString = ['%s/%0',stringLength,'d_%0',stringLength,'d'];
+% old name-based neighbor selection - wont work for 2m
+% [path,name,ext]=fileparts(fileName);
+% 
+% if isempty(path)
+%     path='.';
+% end
+% 
+% a = strsplit(name,'_');
+% stringLength=num2str(length(a{1}));
+% tileRow=str2num(a{1});
+% tileCol=str2num(a{2});
+% 
+% formatString = ['%s/%0',stringLength,'d_%0',stringLength,'d'];
 
 % above
-file1 = [sprintf(formatString,path,tileRow+1,tileCol),...
-sprintf('_%s',a{3:end}),ext];
+% file1 = [sprintf(formatString,path,tileRow+1,tileCol),...
+% sprintf('_%s',a{3:end}),ext];
+file1 = neighborFiles{1};
 if exist(file1,'file')
     m1=matfile(file1);
     dzbuff = getdzbuff(m0,m1);
-    dz0(1,:) = imresize(nanmean(dzbuff),resizeFraction);
-else
-    dz0(1,:) = 0;
+    dzbuff = nanmean(dzbuff);
+    dzbuff = fillmissing(dzbuff,'nearest');
+    dz0(1,:) = imresize(dzbuff,resizeFraction);
 end
 
 % below
-file1 = [sprintf(formatString,path,tileRow-1,tileCol),...
-sprintf('_%s',a{3:end}),ext];
+% file1 = [sprintf(formatString,path,tileRow-1,tileCol),...
+% sprintf('_%s',a{3:end}),ext];
+file1 = neighborFiles{2};
 if exist(file1,'file')
     m1=matfile(file1);
     dzbuff = getdzbuff(m0,m1);
-    dz0(end,:) =imresize(nanmean(dzbuff),resizeFraction);
-else
-    dz0(end,:) = 0;
+    dzbuff = nanmean(dzbuff);
+    dzbuff = fillmissing(dzbuff,'nearest');
+    dz0(end,:)= imresize(dzbuff,resizeFraction);
 end
 
+N0 = uint8(~isnan(dz0));
+
 % left
-file1 = [sprintf(formatString,path,tileRow,tileCol-1),...
-sprintf('_%s',a{3:end}),ext];
+% file1 = [sprintf(formatString,path,tileRow,tileCol-1),...
+% sprintf('_%s',a{3:end}),ext];
+file1 = neighborFiles{3};
 if exist(file1,'file')
     m1=matfile(file1);
     dzbuff = getdzbuff(m0,m1);
-    dz0(:,1) = imresize(nanmean(dzbuff,2),resizeFraction);
-else
-    dz0(:,1) = 0;
+    dzbuff = nanmean(dzbuff,2);
+    dzbuff = fillmissing(dzbuff,'nearest');
+    dz1= imresize(dzbuff,resizeFraction);
+    dz0(:,1)=nanmean([dz1 dz0(:,1)],2);
+    N0(:,1) = N0(:,1) + uint8(~isnan(dz1)); 
 end
 
 % right
-file1 = [sprintf(formatString,path,tileRow,tileCol+1),...
-sprintf('_%s',a{3:end}),ext];
+% file1 = [sprintf(formatString,path,tileRow,tileCol+1),...
+% sprintf('_%s',a{3:end}),ext];
+file1 = neighborFiles{4};
 if exist(file1,'file')
     m1=matfile(file1);
     dzbuff = getdzbuff(m0,m1);
-    dz0(:,end) = imresize(nanmean(dzbuff,2),resizeFraction);
-else
-    dz0(:,end) = 0;
+    dzbuff = nanmean(dzbuff,2);
+    dzbuff = fillmissing(dzbuff,'nearest');
+    dz1= imresize(dzbuff,resizeFraction);
+    dz0(:,end) = nanmean([dz1 dz0(:,end)],2);
+    N0(:,end) = N0(:,end) + uint8(~isnan(dz1)); 
+end
+
+% upper-left
+% file1 = [sprintf(formatString,path,tileRow+1,tileCol-1),...
+% sprintf('_%s',a{3:end}),ext];
+file1 = neighborFiles{5};
+if exist(file1,'file')
+    m1=matfile(file1);
+    dzbuff = getdzbuff(m0,m1);
+    dzbuff = fillmissing(dzbuff,'nearest');
+    dzbuff = fillmissing(dzbuff,'nearest',2);
+    dzbuff = imresize(dzbuff,resizeFraction);
+    
+    szb = size(dzbuff);
+    
+    dzbuff(2:end,2:end) = NaN;
+    
+    dz0sub = dz0(1:szb(1),1:szb(2));
+    N0sub =  single(N0(1:szb(1),1:szb(2)));
+    
+    dz0(1:szb(1),1:szb(2)) = ...
+        ( (dz0sub.*N0sub) + dzbuff ) ./ (N0sub + 1) ;
+end
+
+% upper-right
+% file1 = [sprintf(formatString,path,tileRow+1,tileCol+1),...
+% sprintf('_%s',a{3:end}),ext];
+file1 = neighborFiles{6};
+if exist(file1,'file')
+    m1=matfile(file1);
+    dzbuff = getdzbuff(m0,m1);
+    dzbuff = fillmissing(dzbuff,'nearest');
+    dzbuff = fillmissing(dzbuff,'nearest',2);
+    dzbuff = imresize(dzbuff,resizeFraction);
+    
+    szb = size(dzbuff);
+    
+    dzbuff(2:end,1:end-1) = NaN;
+    
+    dz0sub = dz0(1:szb(1),end-szb(2)+1:end);
+    N0sub =  single(N0(1:szb(1),end-szb(2)+1:end));
+    
+    dz0(1:szb(1),end-szb(2)+1:end) = ...
+        ( (dz0sub.*N0sub) + dzbuff ) ./ (N0sub + 1);
+
+end
+
+% lower-left
+% file1 = [sprintf(formatString,path,tileRow-1,tileCol-1),...
+% sprintf('_%s',a{3:end}),ext];
+file1 = neighborFiles{7};
+if exist(file1,'file')
+    m1=matfile(file1);
+    dzbuff = getdzbuff(m0,m1);
+    dzbuff = fillmissing(dzbuff,'nearest');
+    dzbuff = fillmissing(dzbuff,'nearest',2);
+    dzbuff = imresize(dzbuff,resizeFraction);
+    
+    szb = size(dzbuff);
+    
+    dzbuff(1:end-1,2:end) = NaN;
+    
+    dz0sub = dz0(end-szb(1)+1:end,1:szb(2));
+    N0sub =  single(N0(end-szb(1)+1:end,1:szb(2)));
+    
+    dz0(end-szb(1)+1:end,1:szb(2)) = ...
+        ( (dz0sub.*N0sub) + dzbuff ) ./ (N0sub + 1) ;
+end
+
+% lower-right
+% file1 = [sprintf(formatString,path,tileRow-1,tileCol+1),...
+% sprintf('_%s',a{3:end}),ext];
+file1 = neighborFiles{8};
+if exist(file1,'file')
+    m1=matfile(file1);
+    dzbuff = getdzbuff(m0,m1);
+    dzbuff = fillmissing(dzbuff,'nearest');
+    dzbuff = fillmissing(dzbuff,'nearest',2);
+    dzbuff = imresize(dzbuff,resizeFraction);
+    
+    szb = size(dzbuff);
+    
+    dzbuff(1:end-1,1:end-1) = NaN;
+    
+    dz0sub = dz0(end-szb(1)+1:end,end-szb(2)+1:end);
+    N0sub =  single(N0(end-szb(1)+1:end,end-szb(2)+1:end));
+    
+    dz0(end-szb(1)+1:end,end-szb(2)+1:end) = ...
+        ( (dz0sub.*N0sub) + dzbuff ) ./ (N0sub + 1);
 end
 
 dz0=single(inpaint_nans(double(dz0),2));
-
-dz0 = imresize(dz0,sz);
 
 dz0 = dz0./2;
 
@@ -144,6 +272,7 @@ m0.dz0 = dz0;
 m0.adjusted = false;
 
 function dzbuff=getdzbuff(m0,m1)
+% calulate the offset over the tile buffer
 
 c0 = m0.x >= min(m1.x) & m0.x <= max(m1.x);
 r0 = m0.y >= min(m1.y) & m0.y <= max(m1.y);
@@ -158,6 +287,18 @@ c1 = [find(c1,1,'first'),find(c1,1,'last')];
 r1 = [find(r1,1,'first'),find(r1,1,'last')];
 
 dzbuff = m0.z(r0(1):r0(2),c0(1):c0(2)) - m1.z(r1(1):r1(2),c1(1):c1(2));
+
+s=whos(m0);
+if any(strcmp({s.name},'land'))
+    land = m0.land(r0(1):r0(2),c0(1):c0(2));
+    dzbuff(~land) = NaN;
+end
+
+s=whos(m1);
+if any(strcmp({s.name},'land'))
+    land = m1.land(r0(1):r0(2),c0(1):c0(2));
+    dzbuff(~land) = NaN;
+end
 
         
         
