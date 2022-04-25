592e5c90b07104f88630d389725edd28858a9b26
Erik Husby <20846681+ehusby@users.noreply.github.com>
2022-04-25 11:06:38 -0500

    Add regex parsers for tilename parts

---
+++ parseTileNameParts.m
@@ -0,0 +1,31 @@
+function [prefix, supertile, quadtile, suffix] = parseTileNameParts(tilename, allow_suffix)
+
+prefix = [];
+supertile = [];
+quadtile = [];
+suffix = [];
+
+tilename_re = '(?<prefix>utm\d{2}[ns]_)?(?<supertile>\d{2}_\d{2})(?<quadtile>_\d_\d)?';
+if allow_suffix
+    tilename_re = ['^', tilename_re, '(?<suffix>.*$)'];
+else
+    tilename_re = ['^', tilename_re, '$'];
+end
+
+[tokens, match_idx] = regexp(tilename, tilename_re, 'names');
+if isempty(match_idx)
+    error("Cannot parse tilename parts with regex '%s' from input tilename string: %s", tilename_re, tilename);
+end
+
+if isfield(tokens, 'prefix') && ~isempty(tokens.prefix)
+    prefix = strip(tokens.prefix, 'right', '_');
+end
+if isfield(tokens, 'supertile') && ~isempty(tokens.supertile)
+    supertile = tokens.supertile;
+end
+if isfield(tokens, 'quadtile') && ~isempty(tokens.quadtile)
+    quadtile = strip(tokens.quadtile, 'left', '_');
+end
+if isfield(tokens, 'suffix') && ~isempty(tokens.suffix)
+    suffix = tokens.suffix;
+end
---
+++ parseTileNameWithSuffix.m
@@ -0,0 +1,45 @@
+function [parsed_tilename, supername, quadname, suffix] = parseTileNameWithSuffix(tilename)
+
+allow_suffix = true;
+
+prefix = [];
+supertile = [];
+quadtile = [];
+suffix = [];
+
+tilename_re = '(?<prefix>utm\d{2}[ns]_)?(?<supertile>\d{2}_\d{2})(?<quadtile>_\d_\d)?';
+if allow_suffix
+    tilename_re = ['^', tilename_re, '(?<suffix>.*$)'];
+else
+    tilename_re = ['^', tilename_re, '$'];
+end
+
+[tokens, match_idx] = regexp(tilename, tilename_re, 'names');
+if isempty(match_idx)
+    error("Cannot parse tilename parts with regex '%s' from input tilename string: %s", tilename_re, tilename);
+end
+
+if isfield(tokens, 'prefix') && ~isempty(tokens.prefix)
+    prefix = strip(tokens.prefix, 'right', '_');
+end
+if isfield(tokens, 'supertile') && ~isempty(tokens.supertile)
+    supertile = tokens.supertile;
+end
+if isfield(tokens, 'quadtile') && ~isempty(tokens.quadtile)
+    quadtile = strip(tokens.quadtile, 'left', '_');
+end
+if isfield(tokens, 'suffix') && ~isempty(tokens.suffix)
+    suffix = tokens.suffix;
+end
+
+tilename_parts = {prefix, supertile, quadtile};
+tilename_parts(cellfun(@(s) isempty(s), tilename_parts)) = [];
+parsed_tilename = strjoin(tilename_parts, '_');
+
+supername_parts = {prefix, supertile};
+supername_parts(cellfun(@(s) isempty(s), supername_parts)) = [];
+supername = strjoin(supername_parts, '_');
+
+quadname_parts = {prefix, supertile, quadtile};
+quadname_parts(cellfun(@(s) isempty(s), quadname_parts)) = [];
+quadname = strjoin(quadname_parts, '_');
