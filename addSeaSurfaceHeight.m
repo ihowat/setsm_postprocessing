74bb347907baa3284872e35c795311742fbf41a1
Erik Husby <20846681+ehusby@users.noreply.github.com>
2022-07-22 15:57:25 -0500

    Make EGM96 geoid height lookups compatible with older Matlab versions Also fixes potential efficiency issues for both methods of geoid height lookups

--- addSeaSurfaceHeight.m
+++ addSeaSurfaceHeight.m
@@ -26,8 +26,17 @@ if ~any(~land(:))
     return
 end
 
+% Trying to sample sea level at full (2m) resolution may never return!
+% Sample at 100m resolution. EGM96 geoid native resolution is ~15 min anyways.
+xi_min = idivide(int64(x(1,1)), int64(100), 'floor') * 100;
+xi_max = idivide(int64(x(1,end)), int64(100), 'ceil') * 100;
+yi_max = idivide(int64(y(1,1)), int64(100), 'ceil') * 100;
+yi_min = idivide(int64(y(1,end)), int64(100), 'floor') * 100;
+xi = double(xi_min):100:double(xi_max);
+yi = double(yi_max):-100:double(yi_min);
+
 % grid of x and y coords
-[X,Y] = meshgrid(single(x),single(y));
+[X,Y] = meshgrid(single(xi),single(yi));
 
 % convert to lat lon coordinates
 if epsg == 3412
@@ -36,8 +45,23 @@ elseif epsg == 3031
     [LAT,LON]=polarstereo_inv(X,Y,[],[],-71,0);
 end
 
-% egm96 sea level height above 
-ellipsoidHeight = egm96geoid(LAT,LON);
+% egm96 sea level height above
+fprintf('retrieving ellipsoid heights\n')
+try
+    % Note that older matlab versions egm96geoid don't support
+    % egm96geoid(LAT,LON) syntax and will error.
+    ellipsoidHeight = egm96geoid(LAT,LON);
+catch ME
+    warning('Caught error in egm96geoid method')
+    fprintf(1,'Error identifier: %s\n',ME.identifier);
+    fprintf(1,'Error message:\n%s\n',ME.message);
+    fprintf(1, 'Trying geoidheight method instead\n')
+    ellipsoidHeight = geoidheight(LAT,LON,'egm96');
+    ellipsoidHeight = reshape(ellipsoidHeight, size(LAT));
+end
+fprintf('got ellipsoid heights\n')
+
+ellipsoidHeight = interp2(xi,yi(:),ellipsoidHeight,x,y(:),'*bilinear');
 
 % set heights below the sea level height to sea level height
 M = z < ellipsoidHeight;
@@ -46,6 +70,8 @@ z(M) = ellipsoidHeight(M);
 
 if adaptCoastlineFlag
     % adapt coastline to ice front change/ice bergs
+    fprintf('adapting coastline\n')
+
     M = imdilate(land,ones(100));
     M = M & ( (z-ellipsoidHeight) > 2 );
     
