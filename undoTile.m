5476442370e328f8eac72177281ca541f1f020ef
Erik Husby <20846681+ehusby@users.noreply.github.com>
2022-08-02 16:24:08 -0500

    `undoTile.m`: Refactor so script can work on list of tiles

--- undoTile.m
+++ undoTile.m
@@ -1,42 +1,54 @@
-function undoTile(fileName)
+function undoTile(tileFile_or_list)
 
-m=matfile(fileName);
-m.Properties.Writable = true;
-s=whos(m);
-sz= s(strcmp({s.name},'z')).size;
-% undoBuffer
-
-%undo dz0 addjustment 
-if any(strcmpi(fields(m),'adjusted')) && any(strcmpi(fields(m),'dz0'))
-    if m.adjusted == 1
-        fprintf('undoing adjustment\n')
-        dz0 = imresize(m.dz0,sz);
-        m.z = m.z + dz0;
-        m.adjusted=false;
-    else
-        fprintf('adjusted flag is zero, no change made\n')
-    end
+if iscell(tileFile_or_list)
+    tileFile_list=tileFile_or_list;
 else
-     fprintf('no adjusted and/or dz0 field, no change made\n')
+    tileFile_list={tileFile_or_list};
 end
 
-%undo dzfit
-if any(strcmpi(fields(m),'dzfit')) 
-    %if m.fitted == 1
-    if ~any(strcmpi(fields(m),'dzfitApplied')) || m.dzfitApplied == 1
-        fprintf('undoing dzfit\n')
-        dzfit = imresize(m.dzfit,sz);
-        m.z = m.z + dzfit;
-        m.dzfitApplied=false;
+for tileFile_idx=1:length(tileFile_list)
+    tileFile = tileFile_list{tileFile_idx};
+
+    fprintf("\nWorking on tile file: %s\n", tileFile);
+
+    m=matfile(tileFile);
+    m.Properties.Writable = true;
+    s=whos(m);
+    sz= s(strcmp({s.name},'z')).size;
+    % undoBuffer
+
+    %undo dz0 addjustment
+    if any(strcmpi(fields(m),'adjusted')) && any(strcmpi(fields(m),'dz0'))
+        if m.adjusted == 1
+            fprintf('undoing adjustment\n')
+            dz0 = imresize(m.dz0,sz);
+            m.z = m.z + dz0;
+            m.adjusted=false;
+        else
+            fprintf('adjusted flag is zero, no change made\n')
+        end
     else
-        fprintf('dzfitApplied flag is zero, no change made\n')
+         fprintf('no adjusted and/or dz0 field, no change made\n')
     end
-        
-%     else
-%         fprinft('fitted flag is zero, no change made\n')
-%     end
-else
-     fprintf('no dzfit field, no change made\n')
+
+    %undo dzfit
+    if any(strcmpi(fields(m),'dzfit'))
+        %if m.fitted == 1
+        if ~any(strcmpi(fields(m),'dzfitApplied')) || m.dzfitApplied == 1
+            fprintf('undoing dzfit\n')
+            dzfit = imresize(m.dzfit,sz);
+            m.z = m.z + dzfit;
+            m.dzfitApplied=false;
+        else
+            fprintf('dzfitApplied flag is zero, no change made\n')
+        end
+
+    %     else
+    %         fprinft('fitted flag is zero, no change made\n')
+    %     end
+    else
+         fprintf('no dzfit field, no change made\n')
+    end
 end
 
     
