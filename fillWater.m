62e0ead1e4b5a40cb94a1b1e3a16607d2af6eb8d
Erik Husby <20846681+ehusby@users.noreply.github.com>
2023-05-24 14:51:20 -0500

    `fillWater.m` general improvements - Fix `tileMatFile` arg loading - Apply `M` alterations to `M0` as well - Slight refactoring - Clear arrays when no longer needed

--- fillWater.m
+++ fillWater.m
@@ -40,7 +40,17 @@ end
 
 if any(cellfun(@isempty,{z, z_mad, N, x, y}))
     % read tile DEM mat file
-    m = matfile(tileMatFile);
+    if isstr(tileMatFile) % it's a string, might be a filename
+        if exist(tileMatFile,'file') % yup its a file
+            m = matfile(tileMatFile); % load it
+        else
+            error('tileMatFile does not exist: %s', tileMatFile);
+        end
+    elseif isvalid(tileMatFile) % not a string, is it a valid file handle?
+        m = tileMatFile;
+    else
+        error('tileMatFile arg must be a filename or valid matfile handle');
+    end
     if isempty(z)
         z = m.z;
     end
@@ -77,13 +87,16 @@ M = M0 & ( (z_mad > 0.3 & N > 4 ) | N <= 4 | isnan(z) );
 M0 = bwareaopen(M0, cluster_size_px);
 M = bwareaopen(M, cluster_size_px);
 
+clear C z_mad N;
+
 % remove small non water clusters
 M = ~bwareaopen(~M, cluster_size_px);
+M0 = M0 | M;
 saved_good_z_over_water = xor(M, M0);
 
 % read reference DEM
 R = readGeotiff(refDemFile);
-R.z(R.z < -600) = NaN;
+R.z(R.z < -600) = nan;
 
 % make sure reference DEM is same grid as dem
 R = interp2(R.x,R.y(:),R.z,x,y(:),'*bilinear');
@@ -98,13 +111,15 @@ water_mask_uncertain_zone = xor(imdilate(M, ones(water_mask_uncertain_px*2+1)), 
 % (copernicus generally has good level water surfaces)
 % by sampling the water height a short distance out from land
 % and extending that sample back to shore.
-get_water_height_erode_px = ceil(150 / dx);
-get_water_height_mask = imerode(M, ones(get_water_height_erode_px*2+1));
+get_water_height_dist_from_shore_px = ceil(150 / dx);
+avg_water_height_interp_dist_px = ceil(1000/dx);
+get_water_height_mask = imerode(M, ones(get_water_height_dist_from_shore_px*2+1));
 water_height = nan(size(M));
 water_height(get_water_height_mask) = R(get_water_height_mask);
-water_height = movmean(movmean(water_height, ceil(1000/dx)*2+1, 1, 'omitnan'), ceil(1000/dx)*2+1, 2, 'omitnan');
+water_height = movmean(movmean(water_height, avg_water_height_interp_dist_px*2+1, 1, 'omitnan'), avg_water_height_interp_dist_px*2+1, 2, 'omitnan');
 water_height(get_water_height_mask) = R(get_water_height_mask);
-water_height(water_height > R) = R(water_height > R);
+water_height = min(water_height, R, 'includenan');
+clear get_water_height_mask;
 
 % Add clusters of land to the water mask where the land is within
 % a short distance from the water's edge and the z height is below
@@ -113,8 +128,11 @@ fill_water_minarea_px = ceil(3500 / dx^2);
 fill_water_fillhole_px = ceil(500 / dx^2);
 fill_water = M | (water_mask_uncertain_zone & z < water_height & z < R);
 fill_water = bwareaopen(fill_water, fill_water_minarea_px);
-fill_water = fill_water | (water_mask_uncertain_zone & ~bwareaopen(~fill_water, fill_water_fillhole_px));
-M(water_mask_uncertain_zone & fill_water) = 1;
+fill_water = ~bwareaopen(~fill_water, fill_water_fillhole_px);
+fill_water = fill_water & water_mask_uncertain_zone;
+M(fill_water) = 1;
+M0(fill_water) = 1;
+clear fill_water water_mask_uncertain_zone;
 
 % apply water and no data (ocean) mask to z
 z_masked = z;
@@ -127,13 +145,15 @@ clear z;
 
 % difference map between z_masked and reference DEM with NaNs in voids
 dz = z_masked - R;
+clear z_masked;
 
 % Force water fill to meet reference surface at a set distance from land.
 interp_short_px = ceil(50 / dx);
 interp_long_px = ceil(1000 / dx);
+long_interp_saved_z_dist_from_shore_px = ceil(100 / dx);
 
 force_ref_zone = ~imdilate(~M, ones(interp_short_px*2+1));
-long_interp_area = saved_good_z_over_water & imerode(M0, ones(int32(100/dx)*2+1));
+long_interp_area = saved_good_z_over_water & imerode(M0, ones(long_interp_saved_z_dist_from_shore_px*2+1));
 long_interp_area = imdilate(long_interp_area, ones(interp_long_px*2+1));
 
 shoreline_area = xor(M, imdilate(M, ones(max(3, floor(10/dx)*2+1))));
@@ -150,12 +170,20 @@ dz_interp_short = abs(dz_shoreline) >= 15.0;
 dz_interp_short = bwareaopen(dz_interp_short, 3);
 dz_interp_short_keep = dz_interp_short_keep | dz_interp_short;
 
+clear shoreline_area dz_shoreline dz_interp_short;
+
 long_interp_area(imdilate(dz_interp_short_keep, ones((interp_short_px*2)*2+1))) = 0;
 force_ref_zone(long_interp_area) = 0;
 dz(force_ref_zone) = 0;
 
+clear force_ref_zone long_interp_area dz_interp_short_keep;
+clear saved_good_z_over_water;
+
+
 % interpolate differences across void, breaking dz into tiles to speed up
 A = tile(dz,5,5,200);
+clear dz;
+
 M_tiles = tile(M,5,5,200);
 
 % initiate filled cell array of tiles
@@ -176,19 +204,26 @@ for i=1:numel(A)
         Ai = A{i};
         Ai(water_border) = 0;
         B{i} = single(inpaint_nans(double(Ai),fillWaterInterpMethod));
+        clear Ai water_border;
     end
 end
-clear A;
+clear A M_tiles;
 
 % mosiac filled dz tiles
 dz_filled = retile(B,200,'linear');
+clear B;
 
 % calculate filled DEM
 z_filled = R + dz_filled;
+clear dz_filled;
 
 % Flatten to water level divets in the filled DEM caused by interpolation.
 underwater = M & z_filled < water_height;
 z_filled(underwater) = water_height(underwater);
+M(underwater) = 1;
+clear underwater water_height;
+
+z_nonwater_nans = z_nonwater_nans & ~M;
 
 % Set back to NaN all original NoData that was outside water mask
 % at the beginning of this routine (includes data removed by slope filter).
