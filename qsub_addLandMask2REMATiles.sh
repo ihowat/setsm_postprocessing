5b447263309de1e13c2b4d66d773efd6da42db07
Erik Husby <husby036@umn.edu>
2023-09-06 01:56:51 +0700

    Switch important qsubscripts from PBS to SLURM format

--- qsub_addLandMask2REMATiles.sh
+++ qsub_addLandMask2REMATiles.sh
@@ -1,36 +1,79 @@
 #!/bin/bash
 
-#PBS -l walltime=10:00:00,nodes=1:ppn=2,mem=8gb
-#PBS -m n
-#PBS -k oe
-#PBS -j oe
-#PBS -q old
+##PBS -l walltime=10:00:00,nodes=1:ppn=2,mem=8gb
+##PBS -m n
+##PBS -k oe
+##PBS -j oe
+##PBS -q old
 
-echo ________________________________________________________
+#SBATCH --time 10:00:00
+#SBATCH --nodes 1
+#SBATCH --ntasks 2
+#SBATCH --mem=10G
+#SBATCH --partition=batch
+#SBATCH -o %x.o%j
+
+#echo ________________________________________________________
+#echo
+#echo PBS Job Log
+#echo Start time: $(date)
+#echo
+#echo Job name: $PBS_JOBNAME
+#echo Job ID: $PBS_JOBID
+#echo Submitted by user: $USER
+#echo User effective group ID: $(id -ng)
+#echo
+#echo Hostname of submission: $PBS_O_HOST
+#echo Submitted to cluster: $PBS_SERVER
+#echo Submitted to queue: $PBS_QUEUE
+#echo Requested nodes per job: $PBS_NUM_NODES
+#echo Requested cores per node: $PBS_NUM_PPN
+#echo Requested cores per job: $PBS_NP
+##echo Node list file: $PBS_NODEFILE
+##echo Nodes assigned to job: $(cat $PBS_NODEFILE)
+##echo Running node index: $PBS_O_NODENUM
+#echo
+#echo Running on hostname: $HOSTNAME
+#echo Parent PID: $PPID
+#echo Process PID: $$
+#echo
+#working_dir="$PBS_O_WORKDIR"
+#
+#echo Number of physical cores: $(grep "^core id" /proc/cpuinfo | sort -u | wc -l)
+#echo Number of virtual cores: $(nproc --all)
+#mem_report_cmd="free -g"
+#echo "Memory report [GB] ('${mem_report_cmd}'):"
+#echo -------------------------------------------------------------------------
+#${mem_report_cmd}
+#echo -------------------------------------------------------------------------
+#echo
+
+echo ________________________________________
 echo
-echo PBS Job Log
+echo SLURM Job Log
 echo Start time: $(date)
 echo
-echo Job name: $PBS_JOBNAME
-echo Job ID: $PBS_JOBID
+echo Job name: $SLURM_JOB_NAME
+echo Job ID: $SLURM_JOBID
 echo Submitted by user: $USER
 echo User effective group ID: $(id -ng)
 echo
-echo Hostname of submission: $PBS_O_HOST
-echo Submitted to cluster: $PBS_SERVER
-echo Submitted to queue: $PBS_QUEUE
-echo Requested nodes per job: $PBS_NUM_NODES
-echo Requested cores per node: $PBS_NUM_PPN
-echo Requested cores per job: $PBS_NP
-#echo Node list file: $PBS_NODEFILE
-#echo Nodes assigned to job: $(cat $PBS_NODEFILE)
-#echo Running node index: $PBS_O_NODENUM
+echo SLURM account used: $SLURM_ACCOUNT
+echo Hostname of submission: $SLURM_SUBMIT_HOST
+echo Submitted to cluster: $SLURM_CLUSTER_NAME
+echo Submitted to node: $SLURMD_NODENAME
+echo Cores on node: $SLURM_CPUS_ON_NODE
+echo Requested cores per task: $SLURM_CPUS_PER_TASK
+echo Requested cores per job: $SLURM_NTASKS
+echo Requested walltime: $SBATCH_TIMELIMIT
+#echo Nodes assigned to job: $SLURM_JOB_NODELIST
+#echo Running node index: $SLURM_NODEID
 echo
 echo Running on hostname: $HOSTNAME
 echo Parent PID: $PPID
 echo Process PID: $$
 echo
-working_dir="$PBS_O_WORKDIR"
+working_dir="$SLURM_SUBMIT_DIR"
 
 echo Number of physical cores: $(grep "^core id" /proc/cpuinfo | sort -u | wc -l)
 echo Number of virtual cores: $(nproc --all)
