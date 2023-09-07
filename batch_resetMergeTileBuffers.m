810145533079944048f4a5022c68776818c1ab65
Erik Husby <20846681+ehusby@users.noreply.github.com>
2023-09-08 03:28:58 +0700

    Standardize source matfile gathering for resetMergeTileBuffers

--- batch_resetMergeTileBuffers.m
+++ batch_resetMergeTileBuffers.m
@@ -1,22 +1,16 @@
-function batch_resetMergeTileBuffers(fileNames)
-% undoMergeTileBuffers reverts the tile edge buffers to the original values
-%
-% undoMergeTileBuffers(fileNames) replaces the merged tile buffers, created
-% by mergeTileBuffer, with the original values, setting buffer flags to
-% false.
+function batch_resetMergeTileBuffers(tileDir, resolution)
 
-% make cell if not
-if ~iscell(fileNames)
-    fileNames={fileNames};
+regFiles = dir(sprintf('%s/*_%s_reg.mat', tileDir, resolution));
+unregFiles = dir(sprintf('%s/*_%s.mat', tileDir, resolution));
+regFiles = fullfile({regFiles.folder}, {regFiles.name});
+unregFiles = fullfile({unregFiles.folder}, {unregFiles.name});
+%unregFiles = setdiff(unregFiles, strrep(regFiles, '_reg.mat', '.mat'));
+tileFiles = [unregFiles regFiles];
+
+ntiles = length(tileFiles);
+for i = 1:ntiles
+    tileFile = tileFiles{i};
+    fprintf("Working on tile %d of %d: %s\n", i, ntiles, tileFile);
+    resetMergeTileBuffers(tileFile)
 end
 
-%%
-i=1;
-for i =1:length(fileNames)
-    check_file = fileNames{i};
-    fprintf('\nChecking tile file: %s\n', check_file)
-    if ~isfile(check_file)
-        error('Source file does not exist')
-    end
-    resetMergeTileBuffers(check_file)
-end
