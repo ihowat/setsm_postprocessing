a4bf7a1a4e410a1aa09bff3212fb228d3a0917d6
Devin Power <148888391+power720@users.noreply.github.com>
2024-04-22 11:02:50 -0500

    remove tif writing logic from registerTileToCOP30.m

--- registerTileToCOP30.m
+++ registerTileToCOP30.m
@@ -26,14 +26,6 @@ else
     reportOffsetOnly = false;
 end
 
-n = find(strcmpi('writeDebugTifs',varargin));
-if ~isempty(n)
-    writeDebugTifs = true;
-else
-    writeDebugTifs = false;
-%    writeDebugTifs = true;
-end
-
 n = find(strcmpi('debugTifProjstr',varargin));
 if ~isempty(n)
     debugTifProjstr = varargin{n+1};
@@ -54,7 +46,7 @@ fprintf('Loading reference DEM and ESA WorldCover raster\n');
 [z,x,y,z_at_zr_res,I_ref,C] = loadSlopefiltWaterfillArrays(matfile_or_z,x,y,refDemTif,coverTif);
 zr_dx = I_ref.x(2)-I_ref.x(1);
 
-if writeDebugTifs && ~reportOffsetOnly
+if ~reportOffsetOnly
     z_at_zr_res_unreg = z_at_zr_res;
 else
     z_at_zr_res_unreg = [];
@@ -223,135 +215,7 @@ if ~(reportOffsetOnly || registerBlobs)
     z = z - tile_avg_offset;
 end
 
-if writeDebugTifs
-    avg_offset_array_zr_res_tile = tile_avg_offset * ones(size(z_at_zr_res));
-    if isempty(avg_offset_array_zr_res)
-        avg_offset_array_zr_res = avg_offset_array_zr_res_tile;
-    end
-end
-
 if ~reportOffsetOnly
     % Remake returned z_at_zr_res from adjusted z
     [~,~,~,z_at_zr_res,~,~] = loadSlopefiltWaterfillArrays(z,x,y,refDemTif,[],'skipRefDemLoad');
-end
-
-%% Write out debug tifs
-if writeDebugTifs && (isa(matfile_or_z, 'char') || isa(matfile_or_z, 'string'))
-    outNameBase = matfile_or_z;
-    outNameBase = strrep(outNameBase,'_reg.mat','.mat');
-    outNameBase = strrep(outNameBase,'_unreg.mat.bak','.mat');
-
-    tif_format = 'COG';
-%    tif_format = 'GTiff';
-    projstr = debugTifProjstr;
-    co_predictor = '3';
-%    co_predictor = '1';
-    
-    db_dx = 32;
-
-    outNameBase = strrep(outNameBase,'_2m.mat',sprintf('_%dm.mat', db_dx));
-    
-    if db_dx == zr_dx
-        db_x = I_ref.x;
-        db_y = I_ref.y;
-        z_at_db_res_unreg = z_at_zr_res_unreg;
-        z_at_db_res = z_at_zr_res;
-        zr_at_db_res = I_ref.z;
-        avg_offset_array_db_res = avg_offset_array_zr_res;
-        avg_offset_array_db_res_tile = avg_offset_array_zr_res_tile;
-    else
-        db_x0 = floor(I_ref.x(1)  /db_dx) * db_dx;
-        db_x1 = ceil( I_ref.x(end)/db_dx) * db_dx;
-        db_y0 = floor(I_ref.y(end)/db_dx) * db_dx;
-        db_y1 = ceil( I_ref.y(1)  /db_dx) * db_dx;
-        db_x = db_x0:db_dx:db_x1;
-        db_y = db_y1:-db_dx:db_y0;
-        
-        if ~isempty(z_at_zr_res_unreg)
-            z_at_db_res_unreg = interp2(I_ref.x,I_ref.y(:),z_at_zr_res_unreg,db_x,db_y(:),'*bilinear');
-        end
-        z_at_db_res = interp2(I_ref.x,I_ref.y(:),z_at_zr_res,db_x,db_y(:),'*bilinear');
-        zr_at_db_res = interp2(I_ref.x,I_ref.y(:),I_ref.z,db_x,db_y(:),'*bilinear');
-        if ~isempty(avg_offset_array_zr_res)
-            avg_offset_array_db_res = interp2(I_ref.x,I_ref.y(:),avg_offset_array_zr_res,db_x,db_y(:),'*bilinear');
-        end
-        if ~isempty(avg_offset_array_zr_res_tile)
-            avg_offset_array_db_res_tile = interp2(I_ref.x,I_ref.y(:),avg_offset_array_zr_res_tile,db_x,db_y(:),'*bilinear');
-        end
-    end
-
-    if reportOffsetOnly
-        debug_tif_suffix = 'reportOffsetOnly';
-    elseif registerBlobs
-        debug_tif_suffix = 'blobs';
-    else
-        debug_tif_suffix = 'tiles';
-    end
-
-    suffix_list = {debug_tif_suffix};
-    avg_offset_list = {avg_offset_array_db_res};
-    z_at_db_res_list = {z_at_db_res};
-%    if ~reportOffsetOnly
-%        suffix_list{end+1} = 'reportOffsetOnly';
-%        avg_offset_list{end+1} = avg_offset_array_db_res_tile;
-%        z_at_db_res_list{end+1} = z_at_db_res_unreg;
-%    end
-
-    for i = 1:length(suffix_list)
-        debug_tif_suffix = suffix_list{i};
-        avg_offset_array = avg_offset_list{i};
-        z_at_db_res = z_at_db_res_list{i};
-
-        if ~isempty(avg_offset_array)
-            if strcmp(debug_tif_suffix, 'reportOffsetOnly')
-                offset_suffix = 'tiles';
-            else
-                offset_suffix = debug_tif_suffix;
-            end
-            outNameTif = strrep(outNameBase, '.mat', sprintf('_dem_debug-reg_%s_offset.tif', offset_suffix));
-            fprintf('Writing %s\n', outNameTif)
-            if exist(outNameTif,'file')
-                delete(outNameTif);
-            end
-            if true
-                % Round float values to 1/128 meters to greatly improve compression effectiveness
-                avg_offset_array=round(avg_offset_array*128.0)/128.0;
-                avg_offset_array(isnan(avg_offset_array)) = -9999;
-                writeGeotiff(outNameTif,db_x,db_y,avg_offset_array,4,-9999,projstr,'out_format',tif_format,'co_predictor',co_predictor,'cog_overview_resampling','BILINEAR')
-                clear avg_offset_array;
-            end
-        end
-
-        if ~isempty(z_at_db_res)
-
-            outNameTif = strrep(outNameBase, '.mat', sprintf('_dem_debug-reg_%s.tif', debug_tif_suffix));
-            fprintf('Writing %s\n', outNameTif)
-            if exist(outNameTif,'file')
-                delete(outNameTif);
-            end
-            if true
-                % Round float values to 1/128 meters to greatly improve compression effectiveness
-                z_at_db_res_inst=round(z_at_db_res*128.0)/128.0;
-                z_at_db_res_inst(isnan(z_at_db_res_inst)) = -9999;
-                writeGeotiff(outNameTif,db_x,db_y,z_at_db_res_inst,4,-9999,projstr,'out_format',tif_format,'co_predictor',co_predictor,'cog_overview_resampling','BILINEAR')
-                clear z_at_db_res_inst;
-            end
-
-            dz = z_at_db_res - zr_at_db_res;
-
-            outNameTif = strrep(outNameBase, '.mat', sprintf('_demdiff_debug-reg_%s.tif', debug_tif_suffix));
-            fprintf('Writing %s\n', outNameTif)
-            if exist(outNameTif,'file')
-                delete(outNameTif);
-            end
-            if true
-                % Round float values to 1/128 meters to greatly improve compression effectiveness
-                dz=round(dz*128.0)/128.0;
-                dz(isnan(dz)) = -9999;
-                writeGeotiff(outNameTif,db_x,db_y,dz,4,-9999,projstr,'out_format',tif_format,'co_predictor',co_predictor,'cog_overview_resampling','BILINEAR')
-            end
-            clear dz;
-
-        end
-    end
 end
